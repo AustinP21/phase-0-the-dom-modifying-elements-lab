bd7e02af91216b99364495c8e25b9d310259051e
Austin <austinpulley19@yahoo.com>
2022-02-09 12:06:35 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let main = document.querySelector("main#main");
+main.remove();
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "AUSTIN is the champion"
